ada364e2562bd846ec40a0bfe63132c41badfbd2
Alex Ware <alex.ware@gmail.com>
2014-10-19 01:45:19 +0100

    Refactored rendering function into closure
    Only load the template once now.

--- main.go
+++ main.go
@@ -13,16 +13,9 @@ import (
 )
 
 func main() {
-	b, err := ioutil.ReadFile("file.md")
-	if err != nil {
-		log.Fatalln("Ooops")
-	}
-	a := blackfriday.MarkdownCommon(b)
-	fmt.Println(string(a))
-
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", markdownServe)
+	mux.HandleFunc("/", renderPost())
 	n := negroni.Classic()
 	n.UseHandler(mux)
 	n.Run(":3000")
@@ -34,24 +27,31 @@ type markdownPage struct {
 	Content template.HTML
 }
 
-func markdownServe(rw http.ResponseWriter, r *http.Request) {
-	//fmt.Println(path.Split(r.URL.String())
+func renderPost() func(rw http.ResponseWriter, r *http.Request) {
+
 	t, err := template.ParseFiles("templates/index.tmpl")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	a := path.Clean(r.URL.String())
-	fmt.Println(a)
-	f, err := ioutil.ReadFile("markdown" + a + ".md")
-	if err != nil {
-		log.Println("file not found")
-		rw.WriteHeader(404)
-		fmt.Fprintf(rw, "Not Found.")
-		return
+
+	return func(rw http.ResponseWriter, r *http.Request) {
+		a := path.Clean(r.URL.String())
+		f, err := ioutil.ReadFile("markdown" + a + ".md")
+		if err != nil {
+			log.Println("file not found")
+			rw.WriteHeader(404)
+			fmt.Fprintf(rw, "Not Found.")
+			return
+		}
+		c := struct {
+			Title   string
+			Content template.HTML
+		}{
+			a[1:],
+			template.HTML(blackfriday.MarkdownCommon(f)),
+		}
+		if err := t.Execute(rw, c); err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+		}
 	}
-	c := markdownPage{Title: a[1:], Content: template.HTML(blackfriday.MarkdownCommon(f))}
-	if err := t.Execute(rw, c); err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-	}
-	rw.Write(blackfriday.MarkdownCommon(f))
 }
